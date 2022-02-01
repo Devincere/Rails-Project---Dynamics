330a7411cb9e708a182428e98d1ab5ff623a3bc5
Vincent <71180739+devincere@users.noreply.github.com>
2022-02-01 13:25:50 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   devise_for :users
+  get '/home', to: 'home#index'
+  get '/signin', to: 'signin#index'
+  get '/signup', to: 'signup#index'
+  get '/activites', to: 'activities#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
