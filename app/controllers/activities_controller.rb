cd3e50155f26f46b7664bc2772c61b4981001094
Vincent <71180739+devincere@users.noreply.github.com>
2022-02-06 22:55:59 +0000

    update

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,16 +1,45 @@
 class ActivitiesController < ApplicationController
 
   def index 
+    @activities = Activity.where(user_id: current_user.id)
   end
+
+  def show
+    @activity = Activity.find(params[:id])
+  end
+
   def new
     @activity = Activity.new
   end
+
   def create
     @activity = Activity.new(activity_params)
-  
+
+    @activity.user_id=current_user.id
+    if @activity.save
+      redirect_to @activity
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  private
+  def edit
+    @activity = Activity.find(params[:id])
+    unless current_user.id == @activity.user_id
+    redirect_to activities_path, :alert => "You cannot perfom this action"
+    end
+  end
+
+  def update
+    @activity = Activity.find(params[:id])
+
+    if @activity.update(activity_params)
+      redirect_to @activity
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+      private
 
   def activity_params
     params.require(:activity).permit(:name, :datetime, :duration)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_170910) do
+ActiveRecord::Schema.define(version: 2022_02_05_164928) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2022_02_02_170910) do
     t.integer "duration"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
