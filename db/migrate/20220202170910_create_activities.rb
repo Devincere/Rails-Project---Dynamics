aa305e69e61d18cad0c1037f4ca988abceacf959
Vincent <71180739+devincere@users.noreply.github.com>
2022-02-07 18:55:27 +0000

    update

--- db/migrate/20220202170910_create_activities.rb
+++ db/migrate/20220202170910_create_activities.rb
@@ -1,7 +1,7 @@
 class CreateActivities < ActiveRecord::Migration[7.0]
   def change
     create_table :activities do |t|
-      t.string :name
+      t.text :name
       t.date :datetime
       t.integer :duration
 
